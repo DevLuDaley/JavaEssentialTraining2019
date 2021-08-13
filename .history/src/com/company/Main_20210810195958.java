ae3dc7eb96b4bd23f049c610d8e0b20c74c65b40
devludaley <daleyluis@gmail.com>
2021-08-12 21:58:53 -0400

    history

---
+++ .history/src/com/company/Main_20210810195958.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int result = 10 + 12;
+        String answer = "The answer must be " + result;
+        System.out.println(answer);
+    }
+}
