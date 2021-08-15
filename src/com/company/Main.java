bff42afd1767b71910c6c29c96b8d6d996980305
devludaley <daleyluis@gmail.com>
2021-08-15 08:41:49 -0400

    print 2 separate lines

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,14 @@
-package com.company;
+ package com.company;
 
 public class Main {
 
     public static void main(String[] args) {
-        var doubleValue = 10_000_000.53;
+        // int result = 10 + 12;
+        // String answer = "The answer must be " + result;
+        // System.out.println(answer);
+        int myVar =  5;
+        var myVar2 = 10f;
+        System.out.println("Hello from Java, " + args[0] + "!");
+        System.out.println("This is great Lu" + "!");
     }
 }
